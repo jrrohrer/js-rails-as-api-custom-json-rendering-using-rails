5ac9a4f5afa7f027fa50cd5d2b78d7aee821104b
jrrohrer <jessie.rohrer@gmail.com>
2021-07-05 13:46:33 -0400

    commit tree b025530c1a305229cec4900f7b86bcb2cbfcdff2

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,23 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    # to remove data from the hash of objects rendered:
+    # render json: {id: bird.id, name: bird.name, species: bird.species } 
+    # render json: bird.slice(:id, :name, :species)
+    # render json: birds, only: [:id, :name, :species]
+    # render json: birds, except: [:created_at, :updated_at]
+    # render json: birds.to_json(except: [:created_at, :updated_at])
+
+    # basic error messageing:
+    if bird
+      render json: { id: bird.id, name: bird.name, species: bird.species }
+    else
+      render json: { message: 'Bird not found' }
+    end
   end
 end
